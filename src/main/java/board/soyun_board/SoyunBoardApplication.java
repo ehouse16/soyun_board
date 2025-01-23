984d5792cb4e60385606d4f56956917d3dbf347e
Soyun_p <ehouse190@gmail.com>
2025-01-23 20:11:59 +0900

    :tada: 소윤 게시판 프로젝트 시작!

---
+++ src/main/java/board/soyun_board/SoyunBoardApplication.java
@@ -0,0 +1,13 @@
+package board.soyun_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoyunBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoyunBoardApplication.class, args);
+    }
+
+}
