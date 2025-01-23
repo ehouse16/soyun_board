f8ced4c32e8551a05c3bfe5877d856d1d80cf0ac
Soyun_p <ehouse190@gmail.com>
2025-01-23 21:17:00 +0900

    :recycle: Post entity, PostCreateDto 제약조건 추가

--- src/main/java/board/soyun_board/dto/PostCreateDto.java
+++ src/main/java/board/soyun_board/dto/PostCreateDto.java
@@ -1,18 +1,21 @@
 package board.soyun_board.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class PostCreateDto {
-    @NotBlank
+    @NotBlank(message = "제목을 입력하세요")
+    @Size(min = 1, max = 15, message = "제목은 최소 1자, 최대 15자까지 입력할 수 있습니다")
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "내용을 입력하세요")
+    @Size(min = 1, max = 1000, message = "내용은 최소 1자, 최대 1000자까지 입력할 수 있습니다")
     private String content;
 
-    @NotBlank
+    @NotBlank(message = "글쓴이를 임력해주세요")
     private String author;
 }
