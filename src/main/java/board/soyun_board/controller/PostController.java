8e0706e95253335a1af267e876c44a5d305d5e91
Soyun_p <ehouse190@gmail.com>
2025-01-24 15:25:21 +0900

    :sparkles: 게시글 전체 조회, 단건 조회 기능 구현

--- src/main/java/board/soyun_board/controller/PostController.java
+++ src/main/java/board/soyun_board/controller/PostController.java
@@ -8,16 +8,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
+    //게시글 작성
     @PostMapping("/post/write")
     @ResponseStatus(HttpStatus.CREATED)
     public PostResponseDto write(@RequestBody @Valid PostCreateDto postCreateDto) {
         PostResponseDto postresponseDto = postService.write(postCreateDto);
 
         return postresponseDto;
+    }
+
+    //게시글 전체 조회
+    @GetMapping("/posts")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PostResponseDto> read() {
+        List<PostResponseDto> posts = postService.getPosts();
+
+        return posts;
+    }
+
+    //게시글 단건 조회
+    @GetMapping("/posts/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PostResponseDto read(@PathVariable int id) {
+        PostResponseDto postResponseDto = postService.getPost();
+
+        return postResponseDto;
     }
 }
--- src/main/java/board/soyun_board/service/PostService.java
+++ src/main/java/board/soyun_board/service/PostService.java
@@ -9,14 +9,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
     private final PostMapper postMapper;
 
+    //게시글 작성 메서드
     @Transactional
     public PostResponseDto write(PostCreateDto postCreateDto) {
+        if(postCreateDto.getTitle().isEmpty() || postCreateDto.getContent().isEmpty() || postCreateDto.getAuthor() == null){
+            throw new IllegalArgumentException("게시글 작성에 실패하였습니다");
+        }
         Post post = postMapper.toPostFromPostCreateDto(postCreateDto);
 
         postRepository.save(post);
@@ -24,5 +31,29 @@ public class PostService {
         PostResponseDto postResponseDto = postMapper.toPostResponseDtofromPost(post);
 
         return postResponseDto;
+    }
+
+    //게시글 전체 조회 메서드
+    @Transactional(readOnly = true)
+    public List<PostResponseDto> getPosts() {
+        List<Post> posts = postRepository.findAll();
+
+        List<PostResponseDto> responseDtos = new ArrayList<>();
+
+        for(Post post : posts) {
+            responseDtos.add(postMapper.toPostResponseDtofromPost(post));
+        }
+
+        return responseDtos;
+    }
+
+    //게시글 단건 조회
+    @Transactional(readOnly = true)
+    public PostResponseDto getPost() {
+        Post post = postRepository.findById(1L).get();
+
+        PostResponseDto responseDto = postMapper.toPostResponseDtofromPost(post);
+
+        return responseDto;
     }
 }
