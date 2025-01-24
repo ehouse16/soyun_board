cc179b33465c494621107fc3b51207069bb579aa
Soyun_p <ehouse190@gmail.com>
2025-01-24 15:25:50 +0900

    :sparkles: 게시글 전체 조회, 단건 조회 기능 테스트 완료

--- src/test/java/board/soyun_board/controller/PostControllerTest.java
+++ src/test/java/board/soyun_board/controller/PostControllerTest.java
@@ -2,6 +2,7 @@ package board.soyun_board.controller;
 
 import board.soyun_board.dto.PostCreateDto;
 import board.soyun_board.entity.Post;
+import board.soyun_board.mapper.PostMapper;
 import board.soyun_board.repository.PostRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +12,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -29,6 +35,9 @@ class PostControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private PostMapper postMapper;
 
     @Test
     @DisplayName("게시글 작성 완료")
@@ -58,8 +67,8 @@ class PostControllerTest {
     }
 
     @Test
-    @DisplayName("게시글 작성 완료_실패")
-    void 게시글_작성_완료_실패() throws Exception {
+    @DisplayName("게시글 작성 시 제목 글자 수 부족")
+    void 게시글_작성시_제목글자수_부족() throws Exception {
         PostCreateDto postCreateDto = PostCreateDto.builder()
                 .title("제목")
                 .content("")
@@ -73,6 +82,54 @@ class PostControllerTest {
                         .contentType(APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시글 전체 조회")
+    void 게시글_전체_조회() throws Exception {
+        List<PostCreateDto> createDtos = IntStream.range(0,20)
+                .mapToObj(i -> PostCreateDto.builder()
+                        .title("제목 " + i)
+                        .content("내용 " + i)
+                        .author("저자 " + i)
+                        .build())
+                .toList();
+
+        List<Post> posts = createDtos.stream()
+                .map(postMapper::toPostFromPostCreateDto)
+                .toList();
+
+        postRepository.saveAll(posts);
+
+        mockMvc.perform(get("/posts")
+                    .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[19].title").value("제목 19"))
+                .andExpect(jsonPath("$[19].content").value("내용 19"))
+                .andExpect(jsonPath("$[19].author").value("저자 19"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시글 단건 조회")
+    void 게시글_단건_조회() throws Exception{
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .title("제목")
+                .content("내용")
+                .author("저자")
+                .build();
+
+        Post post = postMapper.toPostFromPostCreateDto(postCreateDto);
+
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/{id}", post.getId())
+                    .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("제목"))
+                .andExpect(jsonPath("$.content").value("내용"))
+                .andExpect(jsonPath("$.author").value("저자"))
                 .andDo(print());
     }
 }
--- src/test/java/board/soyun_board/service/PostServiceTest.java
+++ src/test/java/board/soyun_board/service/PostServiceTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -43,5 +46,59 @@ class PostServiceTest {
         assertEquals(savedpost.getTitle(), post.getTitle());
         assertEquals(savedpost.getContent(), post.getContent());
         assertEquals(savedpost.getAuthor(), post.getAuthor());
+    }
+
+    @Test
+    @DisplayName("게시글 저장 시 제목 글자 수 부족")
+    void 게시글_저장시_제목글자수_부족(){
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .title("테스트 제목 1")
+                .content("")
+                .author("테스트 저자 1")
+                .build();
+
+        assertThrows(IllegalArgumentException.class, () ->
+                postService.write(postCreateDto));
+    }
+
+    @Test
+    @DisplayName("게시글 전체 조회")
+    void 게시글_전체_조회(){
+        List<PostCreateDto> createDtos = IntStream.range(0,20)
+                .mapToObj(i -> PostCreateDto.builder()
+                        .title("제목 " + i)
+                        .content("내용 " + i)
+                        .author("저자 " + i)
+                        .build())
+                .toList();
+
+        List<Post> posts = createDtos.stream()
+                .map(postMapper::toPostFromPostCreateDto)
+                .toList();
+
+        postRepository.saveAll(posts);
+
+        assertEquals(posts.size(), postRepository.findAll().size());
+        assertEquals(posts.get(0).getTitle(), postRepository.findAll().get(0).getTitle());
+        assertEquals(posts.get(0).getContent(), postRepository.findAll().get(0).getContent());
+        assertEquals(posts.get(0).getAuthor(), postRepository.findAll().get(0).getAuthor());
+    }
+
+    @Test
+    @DisplayName("게시글 단건 조회")
+    void 게시글_단건_조회(){
+        PostCreateDto createDto = PostCreateDto.builder()
+                .title("제목")
+                .content("내용")
+                .author("저자")
+                .build();
+
+        Post post = postMapper.toPostFromPostCreateDto(createDto);
+
+        postRepository.save(post);
+
+        assertEquals("제목", postRepository.findAll().get(0).getTitle());
+        assertEquals("내용", postRepository.findAll().get(0).getContent());
+        assertEquals("저자", postRepository.findAll().get(0).getAuthor());
     }
 }
