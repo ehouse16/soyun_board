82302427e763a05b3b99ee2e3e429c8cb08ae680
Soyun_p <ehouse190@gmail.com>
2025-02-16 17:13:14 +0900

    :white_check_mark: 배포 테스트16 (회원가입, 로그인)

--- src/main/java/board/soyun_board/config/WebConfig.java
+++ src/main/java/board/soyun_board/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/static/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true);
