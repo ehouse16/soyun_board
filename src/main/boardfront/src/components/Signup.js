37748e53453e8a8840d0007880d899ee28617a7b
Soyun_p <ehouse190@gmail.com>
2025-02-16 16:56:32 +0900

    :white_check_mark: 배포 테스트13 (회원가입, 로그인)

--- src/main/boardfront/src/components/Signup.js
+++ src/main/boardfront/src/components/Signup.js
@@ -11,7 +11,7 @@ const Signup = () => {
     const handleSignup = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:8080/api/auth/signup', {
+            const response = await fetch('http://13.124.180.241:8080/api/auth/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
